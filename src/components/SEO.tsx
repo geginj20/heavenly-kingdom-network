b8011a60531675e329753a0a2c5a5dc7faad8fb5
KIAI-JOSEPH <nissimasher2019@gmail.com>
2026-06-25 20:35:03 +0300

    Add canonical link tag to SEO component

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -20,6 +20,7 @@ export default function SEO({ title, description, image, url }: SEOProps) {
     <Helmet>
       <title>{fullTitle}</title>
       <meta name="description" content={desc} />
+      <link rel="canonical" href={href} />
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={desc} />
       <meta property="og:image" content={img} />
